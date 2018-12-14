1335ab6e1f09e3ee31a5f240405e9802d841a5e7
Daniela Di Iorio <danielad@gmail.com>
2018-12-14 10:29:21 -0800

    Updates

---
+++ precache-manifest.628a329164abed71e293585e767b0a9b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "45f73f3f689b67af4e41",
+    "url": "/mystioreo/inspiration-board/static/css/main.e0ab854a.chunk.css"
+  },
+  {
+    "revision": "45f73f3f689b67af4e41",
+    "url": "/mystioreo/inspiration-board/static/js/main.45f73f3f.chunk.js"
+  },
+  {
+    "revision": "c0fc13bcfb7e850142e4",
+    "url": "/mystioreo/inspiration-board/static/js/1.c0fc13bc.chunk.js"
+  },
+  {
+    "revision": "da91b3e2538867e933ac",
+    "url": "/mystioreo/inspiration-board/static/js/runtime~main.da91b3e2.js"
+  },
+  {
+    "revision": "952991e9890329764ff489b23579de1e",
+    "url": "/mystioreo/inspiration-board/index.html"
+  }
+];
